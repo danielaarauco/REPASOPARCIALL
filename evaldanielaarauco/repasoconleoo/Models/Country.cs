147f63965e10903d4a4f465228e724c3e4c4eab5
Daniela Arauco <79011580+danielaarauco@users.noreply.github.com>
2021-03-19 23:29:28 -0400

    SE CREO EL MODELO

---
+++ evaldanielaarauco/repasoconleoo/Models/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace repasoconleoo.Models
+{
+    public class Country
+    {
+        [Key]
+        public string Name { get; set; }
+        [Required]
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public double Lating { get; set; }
+        public string Timezones { get; set; }
+        public Currency Currencies { get; set; }
+        public string Flag { get; set; }
+
+    }
+}
---
+++ evaldanielaarauco/repasoconleoo/Models/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace repasoconleoo.Models
+{
+    public class Currency
+    {
+        [Key]
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(5,MinimumLength = 1, ErrorMessage = "ingrese entre 1 a 5 caracteres")] 
+        public string Symbol { get; set; }
+
+    }
+}
